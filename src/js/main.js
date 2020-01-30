c2b3b949a63c6f567747518e36b4a0975daba1fe
llflannery <lflannery@seattletimes.com>
2020-01-29 17:42:32 -0800

    unique id is now title,category,actor

--- src/js/main.js
+++ src/js/main.js
@@ -2,41 +2,29 @@ var $ = require('jquery');
 var scriptURL = 'https://script.google.com/macros/s/AKfycbzsbIC6oCOwBGlZpHyeiwzQtcNKmnnzvVlgRZKFOWsfCBDdIWE/exec';
 var votes = require("../../data/predictions.sheet.json");
 
-var total = 0;
 var width = $(".entry").width();
-const obj = {};
+var obj = {};
+var catObj = {};
 var savedPicks = [];
 
-if (getCookie("OscVotes")) {
-  var pickedArray = getCookie("OscVotes");
-  var pickedSplitArray = pickedArray.split(",");
-  // console.log(pickedArray);
 
-  $.each(pickedSplitArray, function(index, element) {
-    // showVoteTallies(elementAfterPipe);
-    // highlight previously picked entry with elementBeforePipe
-  });
-
-} else {
-  console.log("Vote with reckless abandon");
-}
 
 function getCookie(name) {
     var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    console.log(v);
+    // console.log(v);
     return v ? v[2] : null;
 }
 
 function submitHandler(e, entry){
     var title = $( entry ).attr( "data-title" );
     var category = $( entry ).attr( "data-category" );
+    var actor = $( entry ).attr( "data-actor" );
     var thisMovie = $( entry ).attr( "data-id" );
     savedPicks.push(thisMovie + "|" + category);
-    console.log(savedPicks);
+    // console.log(savedPicks);
 
 
     var formData = new FormData();
-    total = total + 1;
 
 
     if ( obj.hasOwnProperty(thisMovie) ) {
@@ -45,12 +33,19 @@ function submitHandler(e, entry){
       obj[thisMovie] = 1;
     }
 
-    console.log(total);
+    if ( catObj.hasOwnProperty( category ) ) {
+      catObj[category] = (catObj[category] + 1);
+    } else {
+      catObj[category] = 1;
+    }
+
+    console.log(catObj);
     console.log(obj);
 
 
     formData.append("vote", title);
     formData.append("category", category);
+    formData.append("actor", actor);
 
     showVoteTallies(category);
 
@@ -63,14 +58,14 @@ function submitHandler(e, entry){
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message));
 
-    getCookie("OscVotes");
+    // getCookie("OscVotes");
 }
 
 
 $( ".entry" ).click(function(a) {
   var thisEntry = this;
   submitHandler(a, thisEntry);
-  // console.log(this);
+  highlightChosenFadeOthers(thisEntry);
 });
 
 
@@ -83,25 +78,36 @@ $( ".entry" ).click(function(a) {
 
 
 $.each(votes, function(index, element) {
-    total = total + 1;
-    var thisMovie = element.vote + element.category;
+    // total = total + 1;
+    var thisMovie = element.vote + element.category + element.actor;
 
     if ( obj.hasOwnProperty(thisMovie) ) {
       obj[thisMovie] = (obj[thisMovie] + 1);
     } else {
       obj[thisMovie] = 1;
     }
+
+    if ( catObj.hasOwnProperty( element.category ) ) {
+      catObj[element.category] = (catObj[element.category] + 1);
+    } else {
+      catObj[element.category] = 1;
+    }
 });
+
+function highlightChosenFadeOthers( chosenEntry ){
+  $( chosenEntry ).closest('.catGroup').find('.completeEntry').css("opacity","0.6");
+  $( chosenEntry ).closest('.catGroup').find(".entry").css("pointer-events","none");
+  $( chosenEntry ).closest('.completeEntry').addClass('voted').css('opacity','1');
+}
 
 
 function showVoteTallies(selectedCategory) {
-  $( `*[data-category="${ selectedCategory }"]` ).addClass("voted").css("opacity","0.6");
-  $( `*[data-category="${ selectedCategory }"]` ).find(".entry").css("pointer-events","none");
-
   for(var propertyName in obj) {
     if( propertyName.includes(selectedCategory) ){
       var value = obj[propertyName];
-      var percentage = (value / total) * 100;
+      var catTotal = catObj[selectedCategory];
+      // console.log(catTotal);
+      var percentage = (value / catTotal) * 100;
       var perVotes = Math.round(percentage);
 
       $('#nom-holder').find(`*[data-id="${ propertyName }"]`).css("background-size",`${percentage}% 100%`);
@@ -110,8 +116,24 @@ function showVoteTallies(selectedCategory) {
   }
 }
 
+if (getCookie("asdasd")) {
+  var pickedArray = getCookie("OscVotes");
+  savedPicks.push(pickedArray);
+  var pickedSplitArray = pickedArray.split(",");
+  // console.log(savedPicks);
 
+  $.each(pickedSplitArray, function(index, element) {
+    var movAndCat = element.split("|");
+    // console.log(movAndCat);
+    showVoteTallies(movAndCat[1]);
+    highlightChosenFadeOthers( $(`*[data-id="${ movAndCat[0] }"]`)  );
+  });
 
-// console.log(total);
-// // console.log(width);
-// console.log(obj);
+} else {
+  console.log("Vote with reckless abandon");
+}
+
+//(getCookie("OscVotes"))
+
+console.log(obj);
+console.log(catObj);
